4142fdcbc551bd22bbef03cbf733ff35f8acb491
Oosher <105375761+Oosher@users.noreply.github.com>
2023-05-12 02:19:59 +0300

    D

--- src/cards/pages/CardDetailsPage.jsx
+++ src/cards/pages/CardDetailsPage.jsx
@@ -1,9 +1,10 @@
-import { CardMedia, Container, Typography } from "@mui/material";
+import { Box, CardMedia, Container, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import PageHeader from "../../components/PageHeader";
 import useCards from "../hooks/useCards";
 import {Map, GoogleApiWrapper } from "google-maps-react";
+import Maps from "../components/card/Maps";
 
 
 export default function CardDetailsPage() {
@@ -18,13 +19,14 @@ export default function CardDetailsPage() {
 
   },[])
 
-  console.log(card?.image.url);
+  console.log(card?.address);
   return (
     <Container >
       <PageHeader
         title="Business details"
         subtitle="Here you can find all the details about specific business"
       />
+    
       <Container sx={{width:"fit-content"}}>
         <Typography>Business name :{card?.title} {card?.subtitle}</Typography>
         <Typography>Website :{card?.web}</Typography>
@@ -32,7 +34,10 @@ export default function CardDetailsPage() {
         <Typography>Business phone number :{card?.phone}</Typography>
         <Typography>Business number :{card?.bizNumber}</Typography>
         <Typography>Business description :{card?.description}</Typography>
-        <iframe src={card?.map} width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <Box sx={{width: "50vw",height: "40vh", // Adjust as needed
+                  margin: "2vh auto",}}>
+          <Maps address={`${card?.address.city} ${card?.address.street} ${card?.address.houseNumber} `} zoom={13} center={[51,-0.09]}/>
+        </Box>
       </Container>
     </Container>
   );
